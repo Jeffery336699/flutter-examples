d0b32d8df1fd5e2442c32f419425815bbcd25f07
jeff <912833721@qq.com>
2025-08-26 20:13:33 +0800

    生物特征认证（指纹、面部解锁等）

--- biometrics/lib/biometrics_verifier.dart
+++ biometrics/lib/biometrics_verifier.dart
@@ -21,6 +21,15 @@ class BiometricsVerifier {
     try {
       didAuthenticate = await auth.authenticate(
         localizedReason: prompt ?? 'Please authenticate with biometrics',
+      //     这两个属性用于配置生物识别认证的行为：
+      //
+      // *   `stickyAuth`:
+      // *   当设置为 `true` 时，如果身份验证对话框弹出后，您的应用转到后台，身份验证过程不会被取消。当应用返回前台时，对话框会重新出现。
+      // *   这主要适用于 Android。
+      //
+      // *   `biometricOnly`:
+      // *   当设置为 `true` 时，它强制只使用生物识别（如指纹或面部识别）进行身份验证。
+      // *   如果设置为 `false`，当生物识别失败时，系统可能会允许用户回退到使用设备密码（PIN、图案或密码）进行验证。
         options: const AuthenticationOptions(
           stickyAuth: true,
           biometricOnly: true,
